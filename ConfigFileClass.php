8ab81a973500bd8b931d8184509dd43cdd0cc33e
Joel Birch <jbirch88655@gmail.com>
2019-05-10 14:14:21 -0700

    Build Base class for reading from files

---
+++ ConfigFileClass.php
@@ -0,0 +1,67 @@
+<?php
+namespace Config;
+
+class ConfigurationFile
+{
+	private $Configurations;
+	function __construct($fileName)
+	{
+		if($this->IsThisAString($fileName))
+		{
+			if($this->DoesFileExist($fileName))
+			{
+				$Configurations = LoadFile($fileName);
+			}else
+			{
+				$Configurations = array();
+			}
+		}else
+		{
+			throw new \Exception("This is not a valid filename");
+		}
+	}
+	
+	private function DoesFileExist($fileName)
+	{
+		if(IsThisAString($fileName))
+		{
+			$file = dirname(__FILE__) . '/' .$fileName;
+			if(file_exists($file))
+			{
+				return true;
+			}else
+			{
+				return false;
+			}
+		}else
+		{
+			return false;
+		}
+	}
+
+	private function IsThisAString($string)
+	{
+		try
+		{
+			$string = (string) $string;
+			return true;
+		}catch
+		{
+			return false;
+		}
+	}
+
+	private function LoadFile($fileName)
+	{
+		try
+		{
+			return parse_ini_file ($fileName);
+		} catch (\Exception $e)
+		{
+			throw new \Exception("Error loading this ini configuration file");
+		}
+	}
+}
+
+
+?>
